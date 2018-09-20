c95da51ca55c2a5543f00413403bad6e0230d8d5
huimingdeng <13570313864@163.com>
2018-09-20 14:16:45 +0800

    Add an example of a configuration file

---
+++ MySQL-learn/config.php
@@ -0,0 +1,11 @@
+<?php 
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'bmnars');
+define('DB_USER', 'genec1');
+define('DB_PWD', 'k0nob1');
+define('DB_PORT', 3306);
+define('DB_TYPE', 'mysql');
+define('DB_CHARSET', 'UTF8');
+
+
+
