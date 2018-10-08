8850c4058ee3b725a049aa4736ee24a237704134
huimingdeng <13570313864@163.com>
2018-10-08 17:34:04 +0800

    add a unfinised python script

---
+++ NN/show-number.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+from sklearn.datasets import load_digits
+# import pylab as pl
+
+# digits = load_digits() # 载入数据集
+# print(digits.data.shape)
+
+# pl.gray()
+# pl.matshow(digits.images[0])
+# pl.show()
+
