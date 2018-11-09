170af77859713a8c8a5c93558a37fec5e9e0137a
huimingdeng <13570313864@163.com>
2018-11-09 17:35:42 +0800

    添加解决方案，剩4题

--- PHPinterview/exam/PHP13Question.php
+++ PHPinterview/exam/PHP13Question.php
@@ -11,6 +11,13 @@ class PHP13Question{
 
 	/**
 	 * 请问同时满足这样条件的数：被10除余9,被9除余8,被8除余7,在100至1000之间,有几个这样的数?
+	 * 思路：
+	 * 		10x + 9 = (9x+9) + x  ...... 被10除余9
+	 * 		(9x+9)+x / 9 则 x = 8 ...... 被9除余8
+	 * 		则 10*8+9 = 89，且 10 与 9 最小公倍数为 90
+	 * 		89 + 90y = (88+88y)+(1+2y) ...... 被8除余7
+	 * 		则 1+2y = 7 y=3
+	 * 		则 90*3+89 = 359
 	 * @return [type] [description]
 	 */
 	public function range100to1000(){
@@ -150,6 +157,138 @@ class PHP13Question{
 			return ($remainder!==0)?($this->Euclid($n,$remainder)):($n);
 		}
 	}
+	/**
+	 * 输入一行字符，分别统计出其中英文字母、空格、数字和其它字符的个数。
+	 * 正则计算
+	 * @param  [type] $string [description]
+	 * @return [type]         [description]
+	 */
+	public function frontlen($string)
+	{
+		$this->ok = [];
+		$number_c = preg_match_all('/\d{1}/', $string, $front);
+		$letter_c = preg_match_all('/[a-zA-Z]{1}/', $string, $letters);
+		$chinese_c = preg_match_all('/[\x{4e00}-\x{9fa5}]{1}/u', $string, $chinese);
+		$spacing_c = 0;
+		$other_have_null_ch_c = strlen($string) - ($number_c + $letter_c + $chinese_c);
+		$str = $this->letters($string);
+		for($i=0;$i<count($str);$i++){
+			if ($str[$i]==' ') {
+				$spacing_c ++;
+			}
+		}
+		$other_c = mb_strlen($string) - ($number_c + $letter_c + $chinese_c + $spacing_c);
+		return $this->ok = array(
+			'number_count' => $number_c,
+			'letter_count' => $letter_c,
+			'chinese_count' => $chinese_c,
+			'spacing_count' => $spacing_c,
+			'other_count' => $other_c
+		);
+	}
+	/**
+	 * 仿 Python 进行字符串切片使用
+	 * @param  [type] $string [description]
+	 * @return [type]         [description]
+	 */
+	public function letters($string){
+		$this->ok = [];
+		for ($i=0; $i < mb_strlen($string,'utf-8'); $i++) { 
+			$letter = mb_substr($string, $i,1,'utf-8');
+			array_push($this->ok,$letter);
+		}
+		return $this->ok;
+	}
+
+	/**
+	 * 求s=a+aa+aaa+aaaa+aa...a的值，其中a是一个数字。例如2+22+222+2222+22222(此时共有5个数相加)，几个数相加有键盘控制。
+	 * @param  [type] $m [description]
+	 * @param  int $n 循环次数
+	 * @return [type]    [description]
+	 */
+	public function addNumberCount($m,$n,$output=false)
+	{
+
+		$i = 0;
+		$sum = 0;
+		$this->ok = [];
+
+		while ( $i < $n) {
+			$sum = $m + $sum * 10;
+			array_push($this->ok,$sum);
+			$i++;
+		}
+		
+		return ($output)?( ($n!=0)?(sprintf("%d = %s \n", array_sum($this->ok), implode(' + ',$this->ok)) ):(sprintf("%d = %s \n", $m, $m)) ):($this->ok);
+	}
+	/**
+	 * 一个数如果恰好等于它的因子之和，这个数就称为"完数"。例如6=1＋2＋3.编程 找出1000以内的所有完数。
+	 * 性质：
+	 * 		所有的完全数都是三角形数
+	 * 		可以表示成连续奇立方数之和
+	 * 		所有的完全数的倒数都是调和数
+	 * 		
+	 * @param  [type] $num [description]
+	 * @return [type]      [description]
+	 */
+	public function perfectNum($end=1000)
+	{
+		$i = 1;
+		$tmp = [];
+		while ($i <= $end) {
+			$divisor = $this->Divisor($i,true);
+			if( !empty($divisor) ){
+				if( array_sum($divisor) === $i){
+					$tmp[$i] = $i;
+				}
+			}
+			$i++;
+		}
+		$this->ok = $tmp;
+		unset($tmp);
+		return $this->ok;
+	}
+	/**
+	 * 计算一个数字的约数
+	 * @param [type]  $num  [description]
+	 * @param boolean $type false:返回所有约数，true:返回自身外的约数
+	 */
+	private function Divisor($num,$type=false){
+		$i = 1;
+		$this->ok = [];
+		if(!$type){
+			while ($i <= $num) {
+				if($num%$i==0){
+					array_push($this->ok,$i);
+				}
+				$i++;
+			}
+		}else{
+			while ($i < $num) {
+				if($num%$i==0){
+					array_push($this->ok,$i);
+				}
+				$i++;
+			}
+		}
+		return $this->ok;
+	}
+	/**
+	 * 获取第n个三角形数，
+	 * @param  [type] $n [description]
+	 * @return [type]    [description]
+	 */
+	public function triangularNumber($n){
+		return ($n*($n+1))/2;
+	}
+	/**
+	 * 一球从100米高度自由落下，每次落地后反跳回原高度的一半；再落下，求它在 第10次落地时，共经过多少米？第10次反弹多高？
+	 * 思路分析：
+	 * 
+	 */
+	public function halfAndhalf($h=100){
+		$one = $h/2;
+	}
 
 	public static function get_instance()
 	{
@@ -166,15 +305,9 @@ class PHP13Question{
 	
 }
 
-// $s = PHP13Question::get_instance()->range100to1000();
-$s = PHP13Question::get_instance()->primeNumber(73,100);
+$p = PHP13Question::get_instance();
 
-// $s = PHP13Question::get_instance()->daffodil();
-// $s = PHP13Question::get_instance()->decompose(199);
-
-// $s = PHP13Question::get_instance()->conditionOperator(62);
-// $s = PHP13Question::get_instance()->GCDandLCM(10,20);
-
+$s = $p->range100to1000();
 
 
 print_r($s);
