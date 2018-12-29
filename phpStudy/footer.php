eb6ecd3fef9812747a8a78e4286111ff7b47a363
huimingdeng <13570313864@163.com>
2018-12-29 15:10:18 +0800

    修改公共头部尾部信息设置

--- phpStudy/footer.php
+++ phpStudy/footer.php
@@ -1,4 +1,4 @@
-	<footer>&copy;by <?php echo $author;?> &nbsp,<?php echo $createTime; ?>, &nbsp;<a href="/"><?php echo $_SERVER['HTTP_HOST'];?></a></footer>
+	<footer>&copy;by <?php echo $author;?> &nbsp,<?php echo $createTime; ?>, &nbsp;<a href="<?php echo $host_url; ?>"><?php echo $host_name;?></a></footer>
 	</div>
 	<script type="text/javascript" src="php/globals/jquery-3.2.1.js"></script>
 	<script type="text/javascript" src="php/globals/bootstrap.js"></script>
--- phpStudy/header.php
+++ phpStudy/header.php
@@ -4,7 +4,11 @@
 	<title><?php echo $pageTitle; ?></title>
 	<meta charset="utf-8">
 	<link rel="stylesheet" type="text/css" href="php/globals/bootstrap.css">
+	<link href="javascript/css/public.css" rel="stylesheet" type="text/css"/>
 	<?php $author='<a href="https://github.com/huimingdeng/" target="_blank" title="author:DHM">DHM(huimingdeng)</a>'; ?>
+	<?php $host_url = ('localhost' == strtolower($_SERVER['HTTP_HOST']))?('http://localhost/phpStudy/'):($_SERVER['HTTP_HOST']); 
+		$host_name = $_SERVER['HTTP_HOST'];
+	?>
 </head>
 <body>
 	<div class="container">
