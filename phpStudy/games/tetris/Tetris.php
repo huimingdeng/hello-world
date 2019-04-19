51b65d9140cc6d44ff36bf76b4248034bd5abc74
huimingdeng <13570313864@163.com>
2019-04-19 10:11:59 +0800

    矩阵初始化

--- phpStudy/games/tetris/Tetris.php
+++ phpStudy/games/tetris/Tetris.php
@@ -7,8 +7,34 @@
  */
 namespace Tetris;
 
-class Teris{
+class Tetris{
 	const GAP = 0; // 空白部分为0
 	const FALL = 1; // 下落的4个格子状态
 	const BLOCK = 2; // 固定后的状态
+	const MATRIX_WIDTH = 25; // 宽25
+	const MATRIX_HEIGHT = 10; // 列10
+	private static $matrix = []; // 矩阵
+	/**
+	 * 构造函数，初始化
+	 */
+	public function __construct(){
+		$a = $this->init();
+		print_r($a);
+	}
+	/**
+	 * 绘制初始化矩阵
+	 * @return matrix 返回初始化矩阵信息
+	 */
+	public function init(){
+		for ($i=0; $i < self::MATRIX_HEIGHT; $i++) { 
+			for ($j=0; $j < self::MATRIX_WIDTH; $j++) { 
+				self::$matrix[$i][$j] = self::GAP;
+			}
+		}
+		return self::$matrix;
+	}
+
+
 }
+
+$tetris = new Tetris();
