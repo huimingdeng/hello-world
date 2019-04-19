e01df1afd1803b7c36260c7c649b115ad80be517
huimingdeng <13570313864@163.com>
2019-04-19 11:09:12 +0800

    补充说明，修改Tetris类，创建入口index

--- phpStudy/games/tetris/Tetris.php
+++ phpStudy/games/tetris/Tetris.php
@@ -8,18 +8,16 @@
 namespace Tetris;
 
 class Tetris{
-	const GAP = 0; // 空白部分为0
-	const FALL = 1; // 下落的4个格子状态
-	const BLOCK = 2; // 固定后的状态
+	
 	const MATRIX_WIDTH = 25; // 宽25
-	const MATRIX_HEIGHT = 10; // 列10
+	const MATRIX_HEIGHT = 20; // 列20
 	private static $matrix = []; // 矩阵
 	/**
 	 * 构造函数，初始化
 	 */
 	public function __construct(){
-		$a = $this->init();
-		print_r($a);
+		$this->init();
+		$this->MatrixRender();
 	}
 	/**
 	 * 绘制初始化矩阵
@@ -28,13 +26,47 @@ class Tetris{
 	public function init(){
 		for ($i=0; $i < self::MATRIX_HEIGHT; $i++) { 
 			for ($j=0; $j < self::MATRIX_WIDTH; $j++) { 
-				self::$matrix[$i][$j] = self::GAP;
+				self::$matrix[$i][$j] = CellStatus::GAP;
 			}
 		}
 		return self::$matrix;
 	}
-
+	/**
+	 * 矩阵表格渲染
+	 */
+	public function MatrixRender(){
+		$table = '<table class=\'table table-bordered\' border=\'0\' cellpadding=\'0\' cellspacing=\'0\'>';
+		$tr = '';
+		
+		for ($i=0; $i < self::MATRIX_WIDTH; $i++) { 
+			$tr .= '<tr>';
+			$td = '';
+			for ($j=0; $j < self::MATRIX_HEIGHT; $j++) { 
+				
+				$cellstatus = self::$matrix[$j][$i];
+				switch ($cellstatus) {
+					case CellStatus::FALL:
+						$td .= '<td class=\'yel\'>'. '</td>';
+						break;
+					case CellStatus::BLOCK:
+						$td .= '<td class=\'gry\'>'. '</td>';
+						break;
+					default:
+						$td .= '<td class=\'blk\'>'. '</td>';
+						break;
+				}
+				
+				
+			}
+			$tr .= $td;
+			$tr .= '</tr>';
+		}
+		$table .= $tr;
+		$table .= '</table>';
+		echo $table;
+	}
 
 }
 
-$tetris = new Tetris();
+include_once 'CellStatus.php';
+
---
+++ phpStudy/games/tetris/index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>俄罗斯方块</title>
+		<meta charset="utf-8">
+		<link rel="stylesheet" href="../minesweeper/assets/css/bootstrap.min.css">
+		<style type="text/css">
+			.yel {background: yellow;}
+			.blk {background: black;}
+			.gry  {background: gray;}
+		</style>
+	</head>
+	<body>
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12">
+					<h1>俄罗斯方块</h1>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-md-4">
+					<?php 
+
+include_once 'Tetris.php';
+use Tetris\Tetris;
+
+$tetris = new Tetris();
+?>
+				</div>
+			</div>
+		</div>
+		
+	</body>
+</html>
+
