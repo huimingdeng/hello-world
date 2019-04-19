82cfa93777d978c6b4a062e262f13e9f3f77c2b9
huimingdeng <13570313864@163.com>
2019-04-19 09:05:32 +0800

    Tetris 参考和知识备注

--- phpStudy/games/tetris/Tetris.php
+++ phpStudy/games/tetris/Tetris.php
@@ -1,4 +1,10 @@
 <?php 
+/**
+ * Tetris : http://www.imooc.com/article/285405
+ * 阅读有感 -- 表格操作
+ * @author huimingdeng
+ * @version 1.0 
+ */
 namespace Tetris;
 
 class Teris{
---
+++ phpStudy/games/tetris/readme.md
@@ -0,0 +1,5 @@
+# Tetris #
+俄罗斯方块
+
+## 参考 ##
+参考文章： [tetris](http://www.imooc.com/article/285405)
