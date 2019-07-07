3473e6095dc88bfb9d5eef353cd6443a539fc2b8
huimingdeng <13570313864@163.com>
2019-07-07 21:30:07 +0800

    完善查询字段

--- import2posts/export.sql
+++ import2posts/export.sql
@@ -6,7 +6,7 @@ SELECT
 		" on ",
 		pub_date,
 		"</p><p>",
-		title_trans,
+		abstract_trans,
 		"</p><p><a href=\"",
 		links,
 		"\" target=\"_blank\">",
@@ -16,9 +16,19 @@ SELECT
 		keyword_trans,
 		"</p>"
 	) AS post_content,
-	'post' AS post_type,
+	abstract_trans AS post_excerpt,
+	'' AS post_date,
+	'' AS post_category,
+	'' AS post_tag,
+	1 AS post_author,
+	'' AS featured_image,
+	'' AS post_slug,
+	0 AS post_parent,
 	'publish' AS post_status,
+	keyword_trans AS Keywords,
 	'closed' AS comment_status,
-	0 AS menu_order
+	keyword_trans AS seo_keywords,
+	abstract_trans AS seo_description,
+	title_trans AS seo_title
 FROM
 	pmc_crispr_v2
