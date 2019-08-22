cd05b6df3169a482c1a9f4a9b079beefc0c5095a
huimingdeng <13570313864@163.com>
2019-08-22 22:44:28 +0800

    简易RESTful创建

--- phpStudy/RESTful/lib/site.php
+++ phpStudy/RESTful/lib/site.php
@@ -1,4 +1,29 @@
 <?php
+namespace RESTFUL;
+
+Class Site {
+
+	private $sites = array(
+		1 => 'TaoBao',
+		2 => 'Google',
+		3 => 'Runoob',
+		4 => 'Baidu',
+		5 => 'Weibo',
+		6 => 'Sina',
+
+	);
+
+	public function getAllSite() {
+		return $this->sites;
+	}
+
+	public function getSite($id) {
+
+		$site = array($id => ($this->sites[$id]) ? $this->sites[$id] : $this->sites[1]);
+		return $site;
+	}
+}
+
 class SiteRestHandler extends SimpleRest {
 
 	function getAllSites() {
